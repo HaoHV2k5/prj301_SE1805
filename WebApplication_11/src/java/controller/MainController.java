47f7efe6d494fe30a9d4b3222564d7df61fec7c7
HaoHV2k5 <magicmath2k5@gmail.com>
2025-04-17 10:55:37 +0700

    slot 11

---
+++ WebApplication_11/src/java/controller/MainController.java
@@ -0,0 +1,138 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import dao.BookDAO;
+import dao.UserDAO;
+import dto.BookDTO;
+import dto.UserDTO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author asus
+ */
+@WebServlet(name = "MainController", urlPatterns = {"/MainController"})
+
+public class MainController extends HttpServlet {
+
+    public UserDTO getUser(String userID) {
+        UserDAO uDAO = new UserDAO();
+        return uDAO.readById(userID);
+    }
+
+    public boolean isValidLogin(String userName, String password) {
+        UserDTO user = getUser(userName);
+        return user != null && user.getPassword().equals(password);
+    }
+    public static String LOGIN_PAGE = "login.jsp";
+    public static String url;
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        try {
+            String action = request.getParameter("action");
+            url = null;
+            if (action == null) {
+                url = LOGIN_PAGE;
+
+            } else {
+               if(action.equals("login")){
+                    String userName = request.getParameter("txtUser");
+                String password = request.getParameter("txtPassword");
+                if (isValidLogin(userName, password)) {
+                    url = "search.jsp";
+                    UserDTO user = getUser(userName);
+                    request.getSession().setAttribute("user", user);
+                } else {
+                    url = "invalid.jsp";
+                }
+               }else if(action.equals("logout")){
+                   request.getSession().invalidate();
+                   url = "login.jsp";
+                   request.setAttribute("message", "invalid userame or password!");
+               
+               }else if(action.equals("search")){
+                   url = "search.jsp";
+                   BookDAO bdao = new BookDAO();
+                   String searchTerm = request.getParameter("searchTerm")+ "";
+                   List<BookDTO> list = bdao.searchByTitle2(searchTerm);
+                   request.setAttribute("books", list);
+                   request.setAttribute("searchTerm", searchTerm);
+               }else if(action.equals("delete")){
+                   BookDAO bdao = new BookDAO();
+                   String id = request.getParameter("id")+"";
+                   bdao.updateQuantityToZero(id);
+                   
+                   
+                   url = "search.jsp";
+                  
+                   String searchTerm = request.getParameter("searchTerm")+ "";
+                   List<BookDTO> list = bdao.searchByTitle2(searchTerm);
+                   request.setAttribute("books", list);
+                   request.setAttribute("searchTerm", searchTerm);
+               }
+
+            }
+        } catch (Exception e) {
+            log("try catch errors: "+e.toString());
+        } finally {
+            RequestDispatcher rd = request.getRequestDispatcher(url);
+            rd.forward(request, response);
+        }
+
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ WebApplication_11/web/search.jsp
@@ -0,0 +1,96 @@
+<%-- 
+    Document   : search
+    Created on : Apr 14, 2025, 9:45:56 AM
+    Author     : asus
+--%>
+
+<%@page import="dto.BookDTO"%>
+<%@page import="java.util.List"%>
+<%@page import="dto.UserDTO"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+                <%@include file="header.jsp" %>
+        <% 
+            UserDTO user = (UserDTO) session.getAttribute("user");
+            if(user != null){
+                %> 
+        
+                     <h1>Welcome <%=user.getFullName()%></h1>
+        <form action="MainController" method="post">
+            <input type="hidden" name="action" value="logout" />
+            <input type="submit" value="logout"  />
+            
+        </form>
+                     <% String searchTerm = request.getAttribute("searchTerm")+"";
+                            
+                         searchTerm = searchTerm.equals("null")?"":searchTerm;
+                     
+                     %>       
+
+        <form action="MainController" method="post">
+            <input type="hidden" name="action" value="search" />
+            Search Value <input type="text" name="searchTerm" value="<%= searchTerm %>"/><br>
+
+            <input type="submit" name="search"/>
+
+
+        </form>
+        <% 
+            if(request.getAttribute("books")!= null ){
+                 List<BookDTO> listBooks = (List<BookDTO>)request.getAttribute("books");
+                 %> 
+        
+                 <table border="1">
+                     <tr>
+                         <td>BookID</td>
+                         <td>Title</td>
+                         <td>Author</td>
+                         <td>Publish Year</td>
+                         <td>Price</td>
+                         <td>Quantit</td>
+                         <td>Action</td>
+                         
+                     </tr>
+                     
+                     <% 
+                       for (BookDTO b : listBooks) {
+                               
+
+                     %>
+                             <tr>
+                         <td><%= b.getBookID()%></td>
+                         <td><%= b.getTitle()%></td>
+                         <td><%= b.getAuthor()%></td>
+                         <td><%= b.getPublishYear()%></td>
+                         <td><%= b.getPrice()%></td>
+                         <td><%= b.getQuantity()%></td>
+                         <td> <a href="MainController?action=delete&id=<%= b.getBookID()%>&searchTerm=<%=searchTerm%>"> <img src="assets/img/delete-file-icon.png" style="height: 25px"></a></td>
+                         
+                     </tr>  
+
+                     <%
+                           }
+                     
+                     %>                     
+                 </table>
+        <%    
+            }
+        %>
+        
+        
+        <%
+            }else{
+        %> <h4>you do no have permittion to accsess this page !</h4> <%
+}
+        %>
+       
+                <%@include file="footer.jsp" %>
+
+    </body>
+</html>
